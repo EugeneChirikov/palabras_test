946d5c702e310a850e7934e24cb66882191d802e
Chirikov Evgeniy <mjxt47@motorola.com>
2013-06-18 10:46:39 +1100

    Initial commit2

---
+++ gen/com/mates120/myword/test/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.mates120.myword.test;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/mates120/myword/test/DatabaseTest.java
@@ -0,0 +1,8 @@
+package com.mates120.myword.test;
+
+//import android.test.ActivityInstrumentationTestCase2;
+import com.mates120.myword.DatabaseHelper;
+
+public class DatabaseTest {
+
+}
